c35f69da47fa8285c28798880b5016760b693321
Federico Di Filitto <federico.difilitto@protonmail.com>
2022-11-20 03:35:43 +0100

    fix(main): sane file names

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,11 @@ use utils::print_vector;
 const HEADER_SIZE: usize = 2;
 
 fn main(){
-    let source_img =  read_image_data("boh.png");
+    let source_img =  read_image_data("input.png");
     let encoded_text = encoder::encode_text("miao", &source_img.bytes);
     write_image("out.png", source_img, encoded_text);
 
-    let source_img = read_image_data("out.png");
+    let source_img = read_image_data("output.png");
     let text = decoder::decode_text(&source_img.bytes);
     println!("{text}");
 
