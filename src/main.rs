42f705ae2ec70f08904bbe20cc3bfac71a0d220d
Federico Di Filitto <federico.difilitto@protonmail.com>
2022-11-23 08:37:28 +0100

    feat(clap): implemented decode and encode cli commands

--- src/main.rs
+++ src/main.rs
@@ -10,39 +10,72 @@ use utils::print_vector;
 
 use crate::png_io::{read_image_data_from_file, write_image};
 
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(author, version, about, long_about = None)]
-struct Args {
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Encode(Encode),
+    Decode(Decode),
+}
+
+#[derive(Args)]
+struct Encode {
    input: String,
    text: String,
    #[arg(short, long)]
    output: Option<String>,
 }
 
+#[derive(Args)]
+struct Decode {
+   input: String,
+}
+
+
+
 
 fn main(){
-    let args = Args::parse();
-    let input = args.input;
-    let output;
-    if let Some(specified_output) = args.output.as_deref() {
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Encode(cmd) => {
+            encode(&cmd.input, &cmd.text, &cmd.output);
+        },
+        Commands::Decode(cmd) => {
+            decode(&cmd.input);
+        },
+    }
+
+}
+
+fn encode(input: &str, text: &str, specified_output: &Option<String>) {
+    let output: String;
+    if let Some(specified_output) = specified_output.as_deref() {
         output = String::from(specified_output);
     }
     else {
         let input_path = Path::new(&input);
-        output = get_output_filename(input_path);
+        output = get_output_filename(&input_path);
     }
     let source_img =  read_image_data_from_file(&input);
     let format = ImageFormat::from_path(&input).unwrap();
-    let encoded_text = encoder::encode_text(&args.text, &source_img.bytes, format);
+    let encoded_text = encoder::encode_text(text, &source_img.bytes, format);
     write_image(&output, source_img, encoded_text);
-
-    let source_img = read_image_data_from_file(&output);
-    let out_text = decoder::decode_text(&source_img.bytes);
-    println!("{out_text}");
 }
 
+fn decode (input: &str) {
+    let source_img = read_image_data_from_file(input);
+    let out_text = decoder::decode_text(&source_img.bytes);
+    println!("{out_text}");
+    
+}
 
 
 fn get_output_filename(input: &Path) -> String {
